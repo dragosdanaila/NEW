332f7b1aefcd14682b3e3ed5d6018b450e1d8a9e
dragosdanaila <93766580+dragosdanaila@users.noreply.github.com>
2021-12-03 16:55:37 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,52 @@
+var img;
+
+const snowFlakeCount = 500;
+const size = 5;
+const gravity = 0.5;
+
+var snowFlakes = [];
+
+function preload(){
+    img = loadImage('../img/winterscene.jpg')
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(1280, 720);
     canvas.parent("p5container");
+
+   
+
+    for(let i = 0; i < snowFlakeCount ; i++){
+    
+    snowFlakes.push({
+        x: random(width),
+        y: random(height)
+    });
+
+    console.log(snowFlakes);
+}
+
+
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    image(img, 0, 0, width, height);
+
+    noStroke();
+    fill(255,255,255);
+    
+    for(let i = 0; i < snowFlakes.length; i++){
+        var snowFlake = snowFlakes[i];
+                ellipse(snowFlake.x,snowFlake.y, size);
+
+                if(snowFlake.y > height+size){
+                    snowFlake.y =-size
+                    }else{
+                        snowFlake.y += gravity;
+                    
+                    }
+    }
+    
+
 }
